ac5dc48a16699ccfdb9401ec5d6eb1eaddfa3dd7
Adam Gastineau <adam@appcannonsoftware.com>
2018-05-24 13:01:40 -0400

    Working database connection and basic display of DB values

--- WebCommerce/Controllers/ProductsController.cs
+++ WebCommerce/Controllers/ProductsController.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebCommerce.Models;
 
 namespace WebCommerce.Controllers
 {
     public class ProductsController : Controller
     {
+        public DatabaseContext context;
+
+        public ProductsController(DatabaseContext context)
+        {
+            this.context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(context.Products.ToList());
         }
 
         public IActionResult ViewProduct(string id)
--- WebCommerce/Models/Product.cs
+++ WebCommerce/Models/Product.cs
@@ -9,7 +9,7 @@ namespace WebCommerce.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public double Price { get; set; }
+        public decimal Price { get; set; }
         public string Description { get; set; }
     }
 }
